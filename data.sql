4652ea437a2202161d8fb9895ceb887f3a78dcef
ponlawat-w <ponlawat_w@cmu.ac.th>
2019-12-13 19:20:39 +0100

    Update Schema

---
+++ data.sql
@@ -0,0 +1,26 @@
+-- Data for table social_medias
+
+-- Data for table social_media_messages
+
+-- Data for table disasters
+
+-- Data for table earthquakes
+
+-- Data for table volcanic_eruptions
+
+-- Data for table tsunamis
+
+-- Data for table inundations
+
+-- Data for table storms
+
+-- Data for table users
+
+-- Data for table citizens
+
+-- Data for table target_sites
+
+-- Data for table messages_sites
+
+-- Data for table site_disasters
+
