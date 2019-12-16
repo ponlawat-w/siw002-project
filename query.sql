e27093b12eff98728f882285442974840993d4db
ponlawat-w <ponlawat_w@cmu.ac.th>
2019-12-16 18:27:59 +0100

    New Data and Schema for Second

---
+++ query.sql
@@ -0,0 +1,100 @@
+--- 1. Which are severe and active disaster in the last week?
+SELECT d.*
+  FROM disasters d
+  WHERE EXTRACT(day FROM (now() - d.incident_datetime)) < 7;
+
+--- 2. List all the organisations and those disasters they are currently involved in?
+SELECT
+    u.id "Organisation ID",
+    u.full_name "Organisation Name",
+    COALESCE(d.type::VARCHAR, 'not involved') "Disaster Type"
+  FROM users u
+  LEFT JOIN assignments a
+    ON u.id = a.user_id
+  LEFT JOIN target_sites t
+    ON a.target_site = t.id
+  LEFT JOIN disasters d
+    ON t.disaster = d.id
+  WHERE u.type = 'organisation'
+  GROUP BY u.id, d.type;
+
+--- 3. How many high-prioritised target sites are waiting for being approved or responded?
+SELECT COUNT(*)
+  FROM target_sites t
+  WHERE t.priority = 3
+    AND t.status IN ('pending', 'waiting');
+
+--- 4. Create view of high-prioritised target sites are waiting for being approved or responded.
+CREATE OR REPLACE VIEW prioritised_waiting_target_sites AS (
+  SELECT *
+  FROM target_sites t
+  WHERE t.priority = 3
+    AND t.status IN ('pending', 'waiting')
+);
+
+--- 5. For each disaster type, how many times it was mentioned in social media?
+SELECT disaster_types.val, (
+    SELECT COUNT(*)
+      FROM disasters d
+      LEFT JOIN target_sites t
+        ON d.id = t.disaster
+      LEFT JOIN social_media_messages smm
+        ON t.id = smm.target_site
+      WHERE d.type = disaster_types.val
+  )
+  FROM (SELECT UNNEST(ENUM_RANGE(NULL::disaster_type))::disaster_type val) disaster_types;
+
+--- 6. How many disaster by type occured in Phuket in last three months (90 days)? Order the result by occurrance descending.
+SELECT d.type "Disaster Type", COUNT(*) "Occurance"
+  FROM disasters d,
+    (SELECT ST_GeomFromText('POLYGON((98.19970916174111 8.002429215766703,98.26013396642861 8.279757032963346,98.44140838049111 8.203646997340972,98.48535369299111 8.029626669965772,98.55676482580361 7.626921611182967,98.49634002111611 7.229286713053067,98.25464080236611 7.229286713053067,98.18322966955361 7.6160323374499255,98.19970916174111 8.002429215766703))', 4326) geo) phuket
+  WHERE ST_Overlaps(d.geo, phuket.geo)
+    AND EXTRACT(days FROM NOW() - d.incident_datetime) < 90
+  GROUP BY d.type
+  ORDER BY "Occurance" DESC;
+
+--- 7. Given earthquake data with epicenter, create disaster data whose affected area is 100km around epicenter.
+--- Epicenter: POINT(98.05244488118797 18.640972349349987)
+--- Severity: 4
+--- Active: Yes
+--- Incident Date Time: Now
+--- Magnitude: 5
+--- Depth: 12km
+INSERT INTO disasters (id, severity, active, incident_datetime, type, geo)
+  VALUES (
+    (SELECT MAX(id) FROM disasters) + 1,
+    4,
+    TRUE,
+    NOW(),
+    'earthquake',
+    ST_Transform(
+      ST_Buffer(
+        ST_Transform(
+          ST_GeomFromText('POINT(98.05244488118797 18.640972349349987)', 4326),
+        32647),
+      100000),
+    4326)
+  );
+INSERT INTO earthquakes (id, magnitude, depth, epicenter) 
+  SELECT d.id, 5.0, 12000, ST_GeomFromText('POINT(98.05244488118797 18.640972349349987)', 4326) FROM disasters d
+    ORDER BY d.id DESC LIMIT 1;
+
+--- 8. In each disaster incident, which target site is the most isolated?
+SELECT
+  d_centroid.disaster_id,
+  (
+    SELECT t.id
+      FROM target_sites t
+      WHERE t.disaster = d_centroid.disaster_id
+      ORDER BY ST_Distance(t.geo, d_centroid.centroid) DESC
+      LIMIT 1
+  ) target_site_id
+FROM (
+  SELECT
+      d.id disaster_id,
+      ST_Centroid(ST_Collect(t.geo)) centroid
+    FROM disasters d
+    JOIN target_sites t
+      ON d.id = t.disaster
+    GROUP BY d.id
+) d_centroid;
--- schema.sql
+++ schema.sql
@@ -18,38 +18,70 @@ CREATE TABLE social_medias (
   CONSTRAINT social_medias_pk PRIMARY KEY (id)
 );
 
-DROP TABLE IF EXISTS social_media_messages CASCADE;
-CREATE TABLE social_media_messages (
-  id                  INTEGER         NOT NULL,
-  message             TEXT            NOT NULL,
-  published_by        VARCHAR(200)    NOT NULL,
-  published_datetime  TIMESTAMP       NOT NULL,
-  social_media        INTEGER         NOT NULL,
-  geo                 GEOMETRY(POINT) NULL,
-  CONSTRAINT social_media_messages_pk               PRIMARY KEY (id),
-  CONSTRAINT social_media_messages_social_media_fk  FOREIGN KEY (social_media)
-    REFERENCES social_medias(id)
-    ON DELETE CASCADE
-);
-
 DROP TABLE IF EXISTS disasters CASCADE;
 CREATE TABLE disasters (
-  id                INTEGER           NOT NULL,
-  severity          SMALLINT          NOT NULL,
-  active            BOOLEAN           NOT NULL DEFAULT true,
-  incident_datetime TIMESTAMP         NOT NULL DEFAULT NOW(),
-  type              disaster_type     NOT NULL,
-  geo               GEOMETRY(POLYGON) NOT NULL,
+  id                INTEGER                 NOT NULL,
+  severity          SMALLINT                NOT NULL,
+  active            BOOLEAN                 NOT NULL DEFAULT true,
+  incident_datetime TIMESTAMP               NOT NULL DEFAULT NOW(),
+  type              disaster_type           NOT NULL,
+  geo               GEOMETRY(POLYGON, 4326) NOT NULL,
   CONSTRAINT disasters_pk             PRIMARY KEY (id),
   CONSTRAINT disasters_severity_value CHECK (severity BETWEEN 1 AND 5)
 );
 
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE users (
+  id        INTEGER       NOT NULL,
+  full_name VARCHAR(200)  NOT NULL,
+  phone     VARCHAR(50)   NOT NULL,
+  type      user_type     NOT NULL,
+  CONSTRAINT users_pk PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS target_sites CASCADE;
+CREATE TABLE target_sites (
+  id            INTEGER             NOT NULL,
+  status        target_site_status  NOT NULL,
+  priority      INTEGER             NOT NULL,
+  last_updated  TIMESTAMP           NOT NULL  DEFAULT NOW(),
+  created_by    INTEGER             NOT NULL,
+  disaster      INTEGER             NOT NULL,
+  geo           GEOMETRY            NOT NULL,
+  CONSTRAINT target_sites_pk              PRIMARY KEY (id),
+  CONSTRAINT target_sites_created_by_fk   FOREIGN KEY (created_by)
+    REFERENCES users(id)
+    ON DELETE CASCADE,
+  CONSTRAINT target_sites_disaster_fk     FOREIGN KEY (disaster)
+    REFERENCES disasters(id)
+    ON DELETE CASCADE,
+  CONSTRAINT target_sites_priority_value  CHECK (priority BETWEEN 1 AND 3)
+);
+
+DROP TABLE IF EXISTS social_media_messages CASCADE;
+CREATE TABLE social_media_messages (
+  id                  INTEGER               NOT NULL,
+  message             TEXT                  NOT NULL,
+  published_by        VARCHAR(200)          NOT NULL,
+  published_datetime  TIMESTAMP             NOT NULL,
+  social_media        INTEGER               NOT NULL,
+  target_site         INTEGER               NULL,
+  geo                 GEOMETRY(POINT, 4326) NULL,
+  CONSTRAINT social_media_messages_pk               PRIMARY KEY (id),
+  CONSTRAINT social_media_messages_social_media_fk  FOREIGN KEY (social_media)
+    REFERENCES social_medias(id)
+    ON DELETE CASCADE,
+  CONSTRAINT social_media_messages_target_site_fk   FOREIGN KEY (target_site)
+    REFERENCES target_sites(id)
+    ON DELETE CASCADE
+);
+
 DROP TABLE IF EXISTS earthquakes CASCADE;
 CREATE TABLE earthquakes (
-  id        INTEGER         NOT NULL,
-  magnitude REAL            NOT NULL,
-  depth     REAL            NOT NULL,
-  epicenter GEOMETRY(POINT) NOT NULL,
+  id        INTEGER               NOT NULL,
+  magnitude REAL                  NOT NULL,
+  depth     REAL                  NOT NULL,
+  epicenter GEOMETRY(POINT, 4326) NOT NULL,
   CONSTRAINT earthquakes_pk     PRIMARY KEY (id),
   CONSTRAINT earthquakes_id_fk  FOREIGN KEY (id) REFERENCES disasters(id)
     ON DELETE CASCADE
@@ -57,8 +89,8 @@ CREATE TABLE earthquakes (
 
 DROP TABLE IF EXISTS volcanic_eruptions CASCADE;
 CREATE TABLE volcanic_eruptions (
-  id      INTEGER         NOT NULL,
-  crater  GEOMETRY(POINT) NOT NULL,
+  id      INTEGER               NOT NULL,
+  crater  GEOMETRY(POINT, 4326) NOT NULL,
   CONSTRAINT volcanic_eruptions_pk    PRIMARY KEY (id),
   CONSTRAINT volcanic_eruptions_id_fk FOREIGN KEY (id) REFERENCES disasters(id)
     ON DELETE CASCADE
@@ -66,9 +98,9 @@ CREATE TABLE volcanic_eruptions (
 
 DROP TABLE IF EXISTS tsunamis CASCADE;
 CREATE TABLE tsunamis (
-  id              INTEGER               NOT NULL,
-  max_wave_height INTEGER               NOT NULL,
-  coast_line      GEOMETRY(LINESTRING)  NOT NULL,
+  id              INTEGER                     NOT NULL,
+  max_wave_height INTEGER                     NOT NULL,
+  coast_line      GEOMETRY(LINESTRING, 4326)  NOT NULL,
   CONSTRAINT tsunamis_pk    PRIMARY KEY (id),
   CONSTRAINT tsunamis_id_fk FOREIGN KEY (id) REFERENCES disasters(id)
     ON DELETE CASCADE
@@ -85,72 +117,32 @@ CREATE TABLE inundations (
 
 DROP TABLE IF EXISTS storms CASCADE;
 CREATE TABLE storms (
-  id              INTEGER               NOT NULL,
-  max_wind_speed  DOUBLE PRECISION      NOT NULL,
-  path            GEOMETRY(LINESTRING)  NOT NULL,
+  id              INTEGER                     NOT NULL,
+  max_wind_speed  DOUBLE PRECISION            NOT NULL,
+  path            GEOMETRY(LINESTRING, 4326)  NOT NULL,
   CONSTRAINT storms_pk    PRIMARY KEY (id),
   CONSTRAINT storms_id_fk FOREIGN KEY (id) REFERENCES disasters(id)
 );
 
-DROP TABLE IF EXISTS users CASCADE;
-CREATE TABLE users (
-  id        INTEGER       NOT NULL,
-  full_name VARCHAR(200)  NOT NULL,
-  phone     VARCHAR(50)   NOT NULL,
-  type      user_type     NOT NULL,
-  CONSTRAINT users_pk PRIMARY KEY (id)
-);
-
 DROP TABLE IF EXISTS citizens CASCADE;
 CREATE TABLE citizens (
-  id          INTEGER     NOT NULL,
-  citizen_id  VARCHAR(50) NOT NULL,
+  id          INTEGER       NOT NULL,
+  citizen_id  VARCHAR(50)   NOT NULL,
   CONSTRAINT citizens_pk    PRIMARY KEY (id),
   CONSTRAINT citizens_id_fk FOREIGN KEY (id) REFERENCES users(id)
     ON DELETE CASCADE
 );
 
-DROP TABLE IF EXISTS target_sites CASCADE;
-CREATE TABLE target_sites (
-  id            INTEGER                       NOT NULL,
-  status        target_site_status            NOT NULL,
-  priority      INTEGER                       NOT NULL,
-  last_updated  TIMESTAMP                     NOT NULL  DEFAULT NOW(),
-  created_by    INTEGER                       NOT NULL,
-  assigned_to   INTEGER                       NULL,
-  geo           GEOMETRY  NOT NULL,
-  CONSTRAINT target_sites_pk              PRIMARY KEY (id),
-  CONSTRAINT target_sites_created_by_fk   FOREIGN KEY (created_by)
-    REFERENCES users(id)
-    ON DELETE CASCADE,
-  CONSTRAINT target_sites_assigned_to_fk  FOREIGN KEY (assigned_to)
-    REFERENCES users(id)
-    ON DELETE CASCADE,
-  CONSTRAINT target_sites_priority_value  CHECK (priority BETWEEN 1 AND 3)
-);
-
-DROP TABLE IF EXISTS messages_sites CASCADE;
-CREATE TABLE messages_sites (
-  message     INTEGER NOT NULL,
+DROP TABLE IF EXISTS assignments CASCADE;
+CREATE TABLE assignments (
   target_site INTEGER NOT NULL,
-  CONSTRAINT messages_sites_pk              PRIMARY KEY (message, target_site),
-  CONSTRAINT messages_sites_message_fk      FOREIGN KEY (message)
-    REFERENCES social_media_messages(id)
-    ON DELETE CASCADE,
-  CONSTRAINT messages_sites_target_site_fk  FOREIGN KEY (target_site)
-    REFERENCES target_sites(id)
-    ON DELETE CASCADE
-);
-
-DROP TABLE IF EXISTS site_disasters CASCADE;
-CREATE TABLE site_disasters (
-  target_site INTEGER NOT NULL,
-  disaster    INTEGER NOT NULL,
-  CONSTRAINT site_disasters_pk              PRIMARY KEY (target_site, disaster),
-  CONSTRAINT site_disasters_target_site_fk  FOREIGN KEY (target_site)
+  user_id     INTEGER NOT NULL,
+  solved      BOOLEAN NOT NULL DEFAULT FALSE,
+  CONSTRAINT assignments_pk             PRIMARY KEY (target_site, user_id),
+  CONSTRAINT assignments_target_site_fk FOREIGN KEY (target_site)
     REFERENCES target_sites(id)
     ON DELETE CASCADE,
-  CONSTRAINT site_disasters_disaster_fk     FOREIGN KEY (disaster)
-    REFERENCES disasters(id)
+  CONSTRAINT assignments_user_fk        FOREIGN KEY (user_id)
+    REFERENCES  users(id)
     ON DELETE CASCADE
 );
